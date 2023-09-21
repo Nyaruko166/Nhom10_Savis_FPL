2dbbf858bb178939c486dfed93b4dfbae9411285
Nyaruko166 <occho1666@gmail.com>
2023-09-21 19:33:20 +0700

    Init

---
+++ src/main/java/com/example/nhom10_savis_fpl/Nhom10SavisFplApplication.java
@@ -0,0 +1,13 @@
+package com.example.nhom10_savis_fpl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nhom10SavisFplApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Nhom10SavisFplApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/nhom10_savis_fpl/Nhom10SavisFplApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nhom10_savis_fpl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nhom10SavisFplApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
